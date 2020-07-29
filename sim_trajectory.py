b49c2741565982ac7166bb5c2aff52a45de87ce2
Tsang-Kai <tsangkaichang@ucla.edu>
2020-07-29 12:54:32 -0700

    clean sim_trajectory.py

--- sim_trajectory.py
+++ sim_trajectory.py
@@ -27,23 +27,16 @@ time_arr = np.zeros([total_sample_number, 1])
 groundtruth = np.zeros([total_sample_number, 2])
 
 data_ekf = np.zeros([total_sample_number, 2])
-error_ekf = np.zeros([total_sample_number, 2])
-
 data_hybrid = np.zeros([total_sample_number, 2])
-error_hybrid = np.zeros([total_sample_number, 2])
-
 data_circular = np.zeros([total_sample_number, 2])
-error_circular = np.zeros([total_sample_number, 2])
-
 data_lie = np.zeros([total_sample_number, 2])
-error_lie = np.zeros([total_sample_number, 2])
 
 output_traj_file = open("result/trajectory.txt", "w")
 
 
 # simulation
 
-agent_1 = Agent.Agent(_theta=-2.0)
+agent_1 = Agent.Agent()
 
 i = 0
 for T in range(num_T):
@@ -61,41 +54,20 @@ for T in range(num_T):
 		data_ekf[i,0] = ekf_x
 		data_ekf[i,1] = ekf_y
 
-		[or_error, loc_error] = agent_1.estimation_error(ekf_theta, ekf_x, ekf_y)
-
-		error_ekf[i,0] += or_error / total_sample_number
-		error_ekf[i,1] += loc_error / total_sample_number
-		
 		# hybrid
 		[hybrid_theta, hybrid_x, hybrid_y] = agent_1.hybrid_estimate.read_estimation()
 		data_hybrid[i,0] = hybrid_x
 		data_hybrid[i,1] = hybrid_y
 
-		[or_error, loc_error] = agent_1.estimation_error(hybrid_theta, hybrid_x, hybrid_y)
-
-		error_hybrid[i,0] += or_error / total_sample_number
-		error_hybrid[i,1] += loc_error / total_sample_number	
-			
 		# circular
 		[circular_theta, circular_x, circular_y] = agent_1.circular_estimate.read_estimation()
 		data_circular[i,0] = circular_x
 		data_circular[i,1] = circular_y
 
-		[or_error, loc_error] = agent_1.estimation_error(circular_theta, circular_x, circular_y)
-
-		error_circular[i,0] += or_error / total_sample_number
-		error_circular[i,1] += loc_error / total_sample_number
-
 		# lie
 		[lie_theta, lie_x, lie_y] = agent_1.lie_estimate.read_estimation()
 		data_lie[i,0] = lie_x
 		data_lie[i,1] = lie_y
-
-		[or_error, loc_error] = agent_1.estimation_error(lie_theta, lie_x, lie_y)
-
-		error_lie[i,0] += or_error / total_sample_number
-		error_lie[i,1] += loc_error / total_sample_number
-
 
 		result_str = '{:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4}\n'.format(agent_1.time, agent_1.position[0], agent_1.position[1], ekf_x, ekf_y, hybrid_x, hybrid_y, circular_x, circular_y, lie_x, lie_y)
 		output_traj_file.write(result_str)
@@ -116,41 +88,20 @@ for T in range(num_T):
 	data_ekf[i,0] = ekf_x
 	data_ekf[i,1] = ekf_y
 
-	[or_error, loc_error] = agent_1.estimation_error(ekf_theta, ekf_x, ekf_y)
-
-	error_ekf[i,0] += or_error / total_sample_number
-	error_ekf[i,1] += loc_error / total_sample_number
-
 	# hybrid
 	[hybrid_theta, hybrid_x, hybrid_y] = agent_1.hybrid_estimate.read_estimation()
 	data_hybrid[i,0] = hybrid_x
 	data_hybrid[i,1] = hybrid_y
-
-	[or_error, loc_error] = agent_1.estimation_error(hybrid_theta, hybrid_x, hybrid_y)
-
-	error_hybrid[i,0] += or_error / total_sample_number
-	error_hybrid[i,1] += loc_error / total_sample_number
 
 	# circular
 	[circular_theta, circular_x, circular_y] = agent_1.circular_estimate.read_estimation()
 	data_circular[i,0] = circular_x
 	data_circular[i,1] = circular_y
 
-	[or_error, loc_error] = agent_1.estimation_error(circular_theta, circular_x, circular_y)
-
-	error_circular[i,0] += or_error / total_sample_number
-	error_circular[i,1] += loc_error / total_sample_number
-
 	# lie
 	[lie_theta, lie_x, lie_y] = agent_1.lie_estimate.read_estimation()
 	data_lie[i,0] = lie_x
 	data_lie[i,1] = lie_y
-
-	[or_error, loc_error] = agent_1.estimation_error(lie_theta, lie_x, lie_y)
-
-	error_lie[i,0] += or_error / total_sample_number
-	error_lie[i,1] += loc_error / total_sample_number
-		
 
 	result_str = '{:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4} {:2.4}\n'.format(agent_1.time, agent_1.position[0], agent_1.position[1], ekf_x, ekf_y, hybrid_x, hybrid_y, circular_x, circular_y, lie_x, lie_y)
 	output_traj_file.write(result_str)
@@ -180,9 +131,6 @@ plt.plot(data_lie[:,0], data_lie[:,1],'--',  color = plot_color['LG-EKF'], linew
 
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
-
-# plt.xlim([-0.6, 0.6])
-# plt.ylim([-0.1, 1.1])
 
 plt.legend()
 plt.savefig('result/trajectory.png')
